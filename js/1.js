c9bcf59843d221f8da21fa925e31baa0e9c3e3e5
aretmbarsukov <karasbobra@gmail.com>
2025-03-27 14:30:17 +0100

    1

---
+++ js/1.js
@@ -0,0 +1,7 @@
+// const a = 5;
+// console.log(a);
+
+// let b = 10;
+// console.log(b);
+// b = 50;
+// console.log(b);
---
+++ js/2.js
@@ -0,0 +1,22 @@
+// const userAge = 15;
+// console.log(userAge);
+// console.log(typeof(userAge));
+
+// const userName = "Artem";
+// console.log(userName);
+// console.log(typeof(userName));
+
+// let namber;
+// console.log(namber);
+
+// const namber = null;
+// console.log(namber);
+// console.log(typeof namber);
+
+// const isStudent = true;
+// console.log(isStudent);
+// console.log(typeof(isStudent));
+
+// console.log(5>3);
+
+// console.log(5<3);
