74e516476311b1e93b4e3082f7f16201d9768c3f
aretmbarsukov <karasbobra@gmail.com>
2025-03-29 10:43:26 +0100

    leson 2

--- js/DZ1.js
+++ js/DZ1.js
@@ -1,36 +1,36 @@
-const age = 16;
-console.log(age);
-console.log(typeof age);
+// const age = 16;
+// console.log(age);
+// console.log(typeof age);
 
-const name = "Artem";
-console.log(name);
-console.log(typeof name);
+// const name = "Artem";
+// console.log(name);
+// console.log(typeof name);
 
-const isStudent = true;
-console.log(isStudent);
-console.log(typeof isStudent);
+// const isStudent = true;
+// console.log(isStudent);
+// console.log(typeof isStudent);
 
-const myString = "Мечты так и остаются мечтами, если к ним не идти";
-console.log(myString);
-console.log(typeof myString);
+// const myString = "Мечты так и остаются мечтами, если к ним не идти";
+// console.log(myString);
+// console.log(typeof myString);
 
-let myNumber = 4;
-myNumber += 10;
-console.log(myNumber);
-console.log(typeof myNumber);  
+// let myNumber = 4;
+// myNumber += 10;
+// console.log(myNumber);
+// console.log(typeof myNumber);  
 
-const myNull = null;
-console.log(myNull);
-console.log(typeof myNull);
+// const myNull = null;
+// console.log(myNull);
+// console.log(typeof myNull);
 
-const userName = prompt("Введіть своє ім’я");
-console.log(userName);
-console.log(typeof userName);
-alert("Ваше ім'я: " + userName);
+// const userName = prompt("Введіть своє ім’я");
+// console.log(userName);
+// console.log(typeof userName);
+// alert("Ваше ім'я: " + userName);
 
-const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
-console.log(userAnswer);
+// const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log(userAnswer);
 
-alert("Увага! Дія може бути небезпечною.");
-const isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
-console.log(isConfirmed);
+// alert("Увага! Дія може бути небезпечною.");
+// const isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
+// console.log(isConfirmed);
---
+++ js/leson2_number.js
@@ -0,0 +1,81 @@
+// const a = 17;
+// console.log(typeof(a));
+// console.log(a);
+
+
+// const a = 4;
+// const b = 22;
+// const result = a + b;
+// console.log(result);
+
+
+// const a = 4;
+// const b = 22;
+// const result = a>b;
+// console.log(result);
+
+
+//===сувора рівність оператор===
+//===сувора НЕрівність оператор===
+// const a = 0;
+// console.log((typeof)a);
+// const b = false;
+// console.log(typeof (b));
+// const result = b===a;
+// console.log(result);
+
+
+// console.log(27 % 3===0);
+
+
+// const userAge = Number (prompt("Скільки вам років?"));
+// console.log(typeof(userAge));
+
+// const age = 18;
+// console.log(typeof(age));
+
+// const resalt = userAge === age;
+// console.log(resalt);
+
+
+// const a = "5";
+// console.log(typeof (a));
+
+// const resalt = Number(a);
+// console.log(typeof(resalt));
+
+
+// const a = "abc";
+// const b = Number(a);
+// console.log(typeof (b));
+// console.log(b);
+
+
+// let a = 5;
+// a+=10;
+// a = a+10;
+// console.log(a);
+
+
+// const a = "12.5abc";
+// const result = Number.parseInt(a);//отримання з рядка цілого числа
+// const result = Number.parseFloat(a);//отримання з рядка дробового числа
+// console.log(result);
+// console.log(typeof(result));
+
+
+// console.log((0.1 + 0.2).toFixed(1));
+// console.log((0.1*10 + 0.2*10) / 10);
+
+// 1) Створити змінні для зберігання температури за Цельсієм та кельвіни. Перевести температуру з Цельсія у Кельвіни за допомогою формули та вивести результат в консоль.
+// const celsius = 50;
+// const kelvin = celsius + 273.15;
+// console.log(kelvin + "K");
+
+
+// 2) Створити змінну для зберігання кількості днів у місяці. Обчислити кількість годин та хвилин у цьому місяці за допомогою оператора множення та вивести результат в консоль.
+// const monthDay = 30;
+// const hours = monthDay * 24;
+// console.log(hours + " " + "h");
+// const minutes = hours * 60;
+// console.log(minutes + " " + "min");
