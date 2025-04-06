4083d2cf24d4b2ee430b68b7f430d9cb522b9507
aretmbarsukov <karasbobra@gmail.com>
2025-04-06 21:07:41 +0200

    dz3

---
+++ js/DZ3.js
@@ -0,0 +1,25 @@
+//Вивести result в консоль. Перевірити тип змінної result= 5+5+’5’
+const result = 5 + 5 + '5';
+console.log(result);
+
+
+//Створіть змінну email з вашою електронною адресою. Напишіть скрипт,
+//який перевіряє чи містить змінна email символ @ і рахує загальну кількість символів. Результат виведіть в консоль.
+const email = "karasbobra@gmail.com"
+console.log(email.includes("@"))
+
+// Записати в окремих змінних кожне слово: My, name, is.Поєднати всі слова в змінну fullName.
+// До змінної fullName додати ‘Viktor’. fullName вивести в консоль.
+const my = "My"
+const name = "name"
+const is = "is"
+const fullName = `${my} ${name} ${is} Victor`
+console.log(fullName);
+
+
+// Створіть змінну userName з ім’ям користувача і змінну payment з сумою до оплати.
+// За допомогою шаблонного рядка виведіть на екран через alert повідомлення на прикладі цього: «Дякуємо, Олександро!
+// До сплати 300 гривень»
+const userName = prompt("Введіть ваше ім'я:")
+const payment = "300грн"
+alert("«" + "Дякуємо," + " " + userName + "!" + " " + "До" + " " + "оплати" + " " + payment + "»")
