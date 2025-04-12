3f10f3aa2fe71c16a9d6fa3e5a7982f17644671a
aretmbarsukov <karasbobra@gmail.com>
2025-04-12 11:19:23 +0200

    12.04.2025

---
+++ js/class/booliam4.1.js
@@ -0,0 +1,38 @@
+// console.log(Boolean());
+// // const userName = prompt("Ведіть ім`я")
+// // console.log(Boolean(userName));
+// console.log(Boolean(false))
+// console.log(Boolean(NaN));
+// const userAge = Number(prompt("Ведіть імя"))
+// console.log(Boolean(userAge))
+// let user
+// console.log(Boolean(user));
+// console.log(Boolean(Boolean(user)));
+// console.log(Boolean(null))
+// console.log(Boolean(0))
+
+
+// логічний оператор і &&
+// якщо всі значення пизводяться до true повертає останнє значення
+// якщо одне з значень false то на ньому і зупинеться
+// console.log(2 && 15);
+// console.log(0 && 7);
+// console.log(true && NaN && "Artem" && 5)
+
+
+// ЛОГІЧНИЙ ОПЕРАТОР АБО ||
+// ЯКЩО ОДНЕ З ЗНАЧЕНЬ ПРИЗВОДИТЬСЯ ДО true ЙОГО І ПОВЕРТАЄ
+// ЯКЩО ВСІ ЗНАЧЕННЯ ПРИЗВОДЯТЬСЯ ДО false ПОВЕРТАЄ ОСТАННЄ ЗНАЧЕННЯ
+// console.log(null || "a" || false || NaN);
+
+// ЛОГІЧНИЙ ОПЕРАТОР НЕ!
+// ПЕРЕТВОРЮЄ И Boolean ЗНАЧЕННЯ НА ПРОТИЛЕЖНЕ
+// console.log(!null && "artem")
+
+// const userName = prompt("Ведіть імя")
+// console.log(!userName && "ви не заповнили імя");
+
+// console.log(null && "Artem" || 15);
+// console.log(!"artem" || 5 && NaN);
+// console.log(null && 5 || !NaN)
+// console.log(15 && "lesson" || !null && 4 || !"artem");
