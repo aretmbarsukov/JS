132980fc0835179b31f8512e046cac5315f80ed6
aretmbarsukov <karasbobra@gmail.com>
2025-04-27 14:18:46 +0200

    27.04.25

---
+++ js/class/6.2.js
@@ -0,0 +1,61 @@
+// for (let i = 0; i <= 10; i += 1){
+//     console.log(i)
+// }
+
+// let i = 10;
+// while (i >= 1) {
+//     console.log(i)
+//     i -= 1
+// }
+
+// let num;
+// do {
+//     num = prompt(`Введіть число більше 10`)
+// } while (num < 10);
+// console.log(num)
+
+// let number;
+// do {
+//   number = prompt(`Введіть номер телеону що найменше 10 цифер`);
+//   if (number === null) {
+//     alert(`Ввод скасовано`);
+//     break;
+//   }
+//   if (number.length === 10 && !isNaN(number)) {
+//       alert(`Ваш номер телефону ${number} прийнято`);
+//       break
+//   } else {
+//       alert(`Неправельно введені данні`)
+//   }
+// }while(true)
+
+// const info = "рядочок";
+// for (let q = 1; q < info.length; q += 1){
+//     console.log(info[q]);
+// }
+
+// const friend = ["богдан", "гліб", "назар", "артем", "нікіта", "ярослав"]
+// console.log(friend.length)
+// for (let i = 0; i < friend.length; i += 1){
+//     console.log(i);
+//     console.log(friend[i])
+//     console.log(`index ${i + 1} -- ${friend[i]}`);
+//     if (friend[i] === "артем") {
+//         console.log(friend[i]);
+//         console.log("Корсистувач існує");
+        
+//     }
+// }
+
+// let sumOddNumbers = 0;
+// let sumEvenNumber = 0;
+// for (let i = 0; i <= 100; i += 1){
+//     if (i % 2 === 0) {
+//         sumEvenNumber += i
+//     } else if (i % 3 === 0) {
+//         sumOddNumbers += i
+//     }
+// }
+// console.log(`Сума прарних чисел ${sumEvenNumber}`);
+// console.log(`Сума непрарних чисел ${sumOddNumbers}`);
+
