50de6af82bab1e0c2d4d69564310a6a6f43f6a2a
aretmbarsukov <karasbobra@gmail.com>
2025-04-27 18:17:22 +0200

    27.04.2025

---
+++ js/DZ/DZ6.js
@@ -0,0 +1,42 @@
+let q = 1;
+while (q <= 10) {
+    console.log(q)
+    q += 1
+}
+
+console.log("");
+
+const number = 20;
+for (let r = 0; r <= number; r += 2) {
+  console.log(r);
+}
+
+console.log("");
+
+for (let f = 1; f <= 10; f += 1){
+    console.log(`7 x ${f} = ${7*f}`)
+}
+
+console.log("");
+
+const n = Number(prompt(`Введіть любе число:`));
+let num = 1;
+while (true) {
+  if (num >= n) {
+    break;
+  }
+  console.log(num);
+  num += 1;
+}
+
+console.log("")
+
+let i = 1;
+while (i <= 20) {
+    if (i % 3 === 0) {
+        i += 1;
+        continue;
+    }
+    console.log(i);
+    i += 1;
+}
